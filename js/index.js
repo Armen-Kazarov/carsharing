3c5321bc9c444f99e5c85b27bb89b092974f9a24
Armen-Kazarov <jonybravocn.jb@gmail.com>
2024-10-23 21:12:26 +0200

    fixed buttons in header, fixed section hero on tablets and mobile, fixed section available models, fixed section with quastions 23.10.2024 21:10

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
   function burgerMenu() {
-    const iconMenu = document.querySelector('.menu__icon-wrapper');
+    const iconMenu = document.querySelector('.burger-menu');
     const menuBody = document.querySelector('.menu__body');
     const menuButtonClose = document.querySelector('.menu__button-close-wrapper');
     const menuItems = document.querySelectorAll('.header-nav-menu-list-item');
@@ -31,36 +31,6 @@ document.addEventListener("DOMContentLoaded", function() {
         document.body.classList.toggle('_lock');
         menuBody.classList.toggle('_active');
       })
-    }
-    
-  
-    // Scroll on click
-    const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-    if (menuLinks.length > 0) {
-      menuLinks.forEach(menuLink => {
-        menuLink.addEventListener("click", onMenuLinkClick);
-      });
-  
-      function onMenuLinkClick(e) {
-        const menuLink = e.target;
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-          const gotoBlock = document.querySelector(menuLink.dataset.goto);
-          const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
-      
-          if (iconMenu.classList.contains('_active')) {
-            document.body.classList.remove('_lock');
-            iconMenu.classList.remove('_active');
-            menuBody.classList.remove('_active');
-          }
-  
-  
-          window.scrollTo({
-            top: gotoBlockValue,
-            behavior: "smooth"
-          });
-          e.preventDefault();
-        }
-      }
     }
   }
   
